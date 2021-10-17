d0bb6fa40de69fb1ed49c5e1828c3f52e5bc9d39
eshun4 <eshunkofi23@gmail.com>
2021-10-16 19:47:03 -0600

    Lesson 5 and some other minor changes

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -2,3 +2,16 @@ function toggleMenu(){
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
+function lastModified(){
+    var x = document.lastModified;
+    let newDate = new Date(x);
+
+    let options = {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        weekday:"long"
+    }
+    var formatted_date = Intl.DateTimeFormat("en-US", options).format(newDate)
+    document.write( formatted_date);
+}
---
+++ lesson5/js/preston.js
@@ -0,0 +1,57 @@
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+
+function lastModified(){
+    var x = document.lastModified;
+    let newDate = new Date(x);
+
+    let options = {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        weekday:"long"
+    }
+    var formatted_date = Intl.DateTimeFormat("en-US", options).format(newDate)
+    document.write( formatted_date);
+}
+
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let curDate = new Date();
+let dayName = dayNames[curDate.getDay()];
+let monthName = months[curDate.getMonth()];
+let year = curDate.getFullYear();
+let currentDate = `${dayName}, ${curDate.getDate()}, ${monthName} ${year}`;
+
+document.getElementById("currentDate").innerHTML = currentDate;
+
+/* display banner only fridays */
+if(dayName === "Friday") {
+    document.getElementById("banner-top").style.display = "block";
+}
